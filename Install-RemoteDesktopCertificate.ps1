29c52bb61c3553e57477ab4f1e07a4c9eba40e4f
Robert M. Toups, Jr <robert.toups@gmail.com>
2019-12-05 14:19:44 -0500

    First working code

---
+++ Install-RemoteDesktopCertificate.ps1
@@ -0,0 +1,144 @@
+#Requires -Version 4
+#Requires -Modules PKI
+#----------------------------------------------------------------------------------------------------------------------#
+# Install-RemoteDesktopCertificate.ps1
+# by Robert M. Toups, Jr.
+# robert@toups.io
+# (c) 2019 Toups Design Bureau
+#----------------------------------------------------------------------------------------------------------------------#
+param(
+  # The Path to the Let's Encrypt PFX file.
+  [Parameter(
+    Position = 0,
+    Mandatory = $true,
+    HelpMessage = "The Path to the Let's Encrypt PFX file."
+  )]
+  [ValidateScript(
+    { Test-Path -Path $_ -PathType 'Leaf' -Filter '*.pfx' }
+  )]
+  [String]
+  $Path,
+
+  # The password for the private key. This is a secure string.
+  [Parameter(
+    Position = 1,
+    Mandatory = $false,
+    HelpMessage = 'The password for the private key. This is a secure string.'
+  )]
+  [ValidateNotNull()]
+  [SecureString]
+  $Password = (Read-Host -Prompt 'Certificate PFX Password' -AsSecureString),
+
+  # The location to store the Let's Encrypt PFX certificate.
+  [Parameter(
+    Position = 2,
+    Mandatory = $false,
+    HelpMessage = "The location to store the Let's Encrypt PFX certificate."
+  )]
+  [ValidateNotNull()]
+  [String]
+  $CertificateStoreLocation = 'Cert:\LocalMachine\My',
+
+  # The distinguished name for Let's Encrypt.
+  [Parameter(
+    Position = 3,
+    Mandatory = $false,
+    HelpMessage = "The distinguished name for Let's Encrypt."
+  )]
+  [ValidateNotNull()]
+  [String]
+  $LetsEncryptDistinguishedName = "CN=Let's Encrypt Authority X3, O=Let's Encrypt, C=US"
+)
+#----------------------------------------------------------------------------------------------------------------------#
+# Let's Be Strict About This
+#----------------------------------------------------------------------------------------------------------------------#
+Set-StrictMode -Version Latest
+#----------------------------------------------------------------------------------------------------------------------#
+# Start the Clock
+#----------------------------------------------------------------------------------------------------------------------#
+$RunTime = [System.Diagnostics.Stopwatch]::StartNew()
+#----------------------------------------------------------------------------------------------------------------------#
+# Obtain Fully Qualified Name and Set the Issuer
+#----------------------------------------------------------------------------------------------------------------------#
+$LocalFqdn = (([System.Net.Dns]::GetHostByName(($env:COMPUTERNAME))).Hostname).ToLower()
+$Subject = "CN=$LocalFqdn"
+#----------------------------------------------------------------------------------------------------------------------#
+# Install Let's Encrypt PFX Certificate into $CertificateStoreLocation
+#----------------------------------------------------------------------------------------------------------------------#
+$ArgumentCollection = @{
+  FilePath          = $Path
+  Password          = $Password
+  CertStoreLocation = $CertificateStoreLocation
+}
+try {
+  $ImportedCertificate = Import-PfxCertificate @ArgumentCollection
+} catch {
+  $SpecificReason = "Failed to import certificate: $Path"
+  $ErrorMessage = $PSItem.Exception.Message
+  throw "($ErrorMessage): $SpecificReason Exiting."
+}
+#----------------------------------------------------------------------------------------------------------------------#
+# Obtain Current Remote Desktop Configuration
+#----------------------------------------------------------------------------------------------------------------------#
+$ArgumentCollection = @{
+  Class       = 'Win32_TSGeneralSetting'
+  Namespace   = 'ROOT/cimv2/terminalservices'
+  Filter      = "TerminalName='RDP-tcp'"
+  ErrorAction = 'Stop'
+}
+try {
+  $RemoteDesktopSetting = Get-WmiObject @ArgumentCollection
+} catch {
+  $SpecificReason = "Failed to obtain the Remote Desktop Configuration."
+  $ErrorMessage = $PSItem.Exception.Message
+  throw "($ErrorMessage): $SpecificReason Exiting."
+}
+#----------------------------------------------------------------------------------------------------------------------#
+# Locate Newly Installed Certificate Thumbprint
+#----------------------------------------------------------------------------------------------------------------------#
+$CertificateThumbprint = Get-ChildItem -Path $CertificateStoreLocation |
+  Where-Object { $_.Issuer -eq $LetsEncryptDistinguishedName -and $_.Subject -eq $Subject } |
+  Select-Object -ExpandProperty 'Thumbprint'
+if($null -eq $CertificateThumbprint) {
+  throw "Unable to locate Let's Encrypt Certificate that was imported."
+}
+#----------------------------------------------------------------------------------------------------------------------#
+# Assign Certificate Thumbprint to Remote Desktop Configuration
+#----------------------------------------------------------------------------------------------------------------------#
+$ArgumentCollection = @{
+  Path        = $RemoteDesktopSetting.__path
+  Argument    = @{SSLCertificateSHA1Hash = "$CertificateThumbprint" }
+  ErrorAction = 'Stop'
+}
+try {
+  $null = Set-WmiInstance @ArgumentCollection
+} catch {
+  $SpecificReason = "Unable to apply the new certificate to the Remote Desktop configuration."
+  $ErrorMessage = $PSItem.Exception.Message
+  throw "($ErrorMessage): $SpecificReason Exiting."
+}
+#----------------------------------------------------------------------------------------------------------------------#
+# Validate Install
+#----------------------------------------------------------------------------------------------------------------------#
+$ArgumentCollection = @{
+  Class       = 'Win32_TSGeneralSetting'
+  Namespace   = 'ROOT/cimv2/terminalservices'
+  Filter      = "TerminalName='RDP-tcp'"
+  ErrorAction = 'Stop'
+}
+try {
+  $RemoteDesktopSetting = Get-WmiObject @ArgumentCollection
+} catch {
+  $SpecificReason = "Failed to obtain the Remote Desktop Configuration."
+  $ErrorMessage = $PSItem.Exception.Message
+  throw "($ErrorMessage): $SpecificReason Exiting."
+}
+if($RemoteDesktopSetting.SSLCertificateSHA1Hash -eq $ImportedCertificate.Thumbprint) {
+  Write-Host "Successfully imported and installed $Path" -ForegroundColor 'Green'
+} else {
+  Write-Host "Failed to import and install $Path" -ForegroundColor 'Yellow'
+}
+#----------------------------------------------------------------------------------------------------------------------#
+# Stop the Clock
+#----------------------------------------------------------------------------------------------------------------------#
+Write-Host "Run Time: $($RunTime.Elapsed.ToString().Split('.')[0])" -ForegroundColor 'Green'
